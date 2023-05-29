8b25cfdb25900a6c00625659cda32493f4faa508
shiv-sg010 <120503738+shiv-sg010@users.noreply.github.com>
2023-05-29 20:42:45 +0530

    Add files via upload

---
+++ django-on-docker/app/home/views.py
@@ -0,0 +1,96 @@
+from typing import Any, Dict
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Wishlist, WishlistItems
+from django.contrib import messages
+from customers.models import Cart
+from vendors.models import Item
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
+
+
+def home(request):
+    return redirect('homepage')
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
+
+def sign_in(request):
+    return render(request, 'home/sign_in.html')
+
+
+def sign_up(request):
+    return render(request, 'home/sign_up.html')
+
+
+def add_to_wishlist(request, id):
+    item = Item.objects.get(id=id)
+    user = request.user
+    wishlist, _ = Wishlist.objects.get_or_create(user=user)
+    f = 0
+    for obj in WishlistItems.objects.filter(wishlist=wishlist):
+        if obj.item == item:
+            f = 1
+    if f == 0:
+        wishlist_item = WishlistItems.objects.create(
+            wishlist=wishlist, item=item,)
+        wishlist_item.save()
+        messages.success(request, f'{"Your item has been added to wishlist!"}')
+    else:
+        messages.info(request, f'{"Item is already in your wishlist!"}')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def remove_wishlist(request, wishlist_item_id):
+    try:
+        wishlist_item = WishlistItems.objects.get(id=wishlist_item_id)
+        wishlist_item.delete()
+    except Exception as e:
+        print(e)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def wishlist(request):
+    if Cart.objects.filter(user=request.user, is_paid=False).exists():
+        obj = Cart.objects.get(user=request.user, is_paid=False)
+        count = obj.get_cart_count
+    else:
+        count = 0
+    if Wishlist.objects.filter(user=request.user).exists():
+        if WishlistItems.objects.filter(wishlist=Wishlist.objects.get(user=request.user)).exists():
+            wishlist_obj = Wishlist.objects.filter(user=request.user)
+            w_obj = Wishlist.objects.get(user=request.user)
+            w_items = WishlistItems.objects.all()
+            wishlistitems = []
+            for product in w_items:
+                for p in wishlist_obj:
+                    if product.wishlist == p:
+                        if product.item.units_available == 0:
+                            p.is_outofstock = True
+                            p.save()
+                        if p.is_outofstock is True:
+                            if product.item.units_available != 0:
+                                p.is_outofstock = False
+                                p.save()
+                                template = render_to_string(
+                                    'home/customer_email.html', {'product': product, 'wishlist_obj': w_obj})
+                                email_customer = EmailMessage(
+                                    'Shopmart.in purchase made', template, settings.EMAIL_HOST_USER, [request.user.email])
+                                email_customer.fail_silently = False
+                                email_customer.send()
+
+                        wishlistitems.append(product)
+            context = {'wishlistitems': wishlistitems,
+                       'wishlist_obj': w_obj, 'count': count}
+            return render(request, 'home/wishlist.html', context)
+        else:
+            messages.info(request, f'{"Your Wishlist is Empty."}')
+            return render(request, 'home/wishlist.html', {'wishlistitems': '', 'wishlist_obj': '', 'count': count})
+    else:
+        messages.info(request, f'{"Your Wishlist is Empty."}')
+        return render(request, 'home/wishlist.html', {'wishlistitems': '', 'wishlist_obj': '', 'count': count})
---
+++ django-on-docker/app/shopmart/settings.py
@@ -0,0 +1,172 @@
+
+
+from django.contrib.messages import constants as messages
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = int(os.environ.get("DEBUG", default=0))
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
+SITE_ID = 1
+SOCIALACCOUNT_LOGIN_ON_GET = True
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'accounts',
+    'home',
+    'customers',
+    'vendors',
+    'crispy_forms',
+    'crispy_bootstrap4',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'import_export',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'shopmart.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'shopmart.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+AUTH_USER_MODEL = "accounts.User"
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email'],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+# SMTP onfiguration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'shopmart2.2023@gmail.com'
+EMAIL_HOST_PASSWORD = 'vbll szrq asjs oobq'
